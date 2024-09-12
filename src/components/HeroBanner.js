2d6abf01aedb11d7d54630665e0760c079f7010b
Rheno001 <renfredreuben@gmail.com>
2024-09-12 20:49:19 +0100

    css file added

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {Box, Stack, Typography, Button} from '@mui/material';
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
-    <Box sx={{
-        mt: {lg: '212px', xs: '70px'}, ml: {sm: '50px'} 
+    <Box className='flex justify-center' sx={{
+        mt: {lg: '50px', xs: '70px'}, ml: {sm: '50px'} 
     }} position= 'relative' p= '20px'>
-        <Typography className='text-green-800' fontWeight="600" fontSize="26px">fitness club</Typography>
-        <Typography fontWeight='700' sx={{fontsize: {lg:'44px', xs: '40px'}}}>Sweat, Smile <br />and Repeat</Typography>
-        <Typography fontSize='22px' lineHeight='35px'>Check out the most effective exercises</Typography>
-        <Button variant='contained' className='bg-green-800'>Explore Exercises</Button>
+        <div className='mt-[100px]'>
+        <Typography className='text-green-800' fontWeight="600" sx= {{ fontSize: {lg: "30px", xs: "25"}}}>Fitness club</Typography>
+        <Typography fontWeight='700' className='text-green-700' sx= {{ fontSize: {lg: "20px", xs: "15"}}}>Sweat, Smile <br />and Repeat</Typography>
+        <Typography className='text-green-800'mb={2} lineHeight='35px' fontWeight='600' sx= {{ fontSize: {lg: "30px", xs: "25"}}}>Check out the most effective exercises</Typography>
+        <Button color='success' variant='contained' href='#exercises' sx={{padding: '15px', fontSize: {lg: '15px', xs: '10px'}}}>Explore Exercises</Button>
+        <Typography fontWeight='600' color='#ff2625' position='absolute'  fontSize='200px' sx={{opacity: 0.1, display: {lg: 'block', xs:'none'}}}>
+          Exercise
+        </Typography>
+        </div>
+        <div><img src={HeroBannerImage} alt="banner" position='relative' className='hero-banner-img h-3/4' /></div>
     </Box>
   )
 }
