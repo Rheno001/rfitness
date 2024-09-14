ee196bf7cd03ec41e9db38f9df3e76916070f7ef
Rheno001 <renfredreuben@gmail.com>
2024-09-14 18:19:17 +0100

    fetching data from rapid api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import ExerciseDetail from './pages/ExerciseDetail';
@@ -7,17 +7,16 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import './App.css';
 
-const App = () => {
-  return (
-    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
-        <Navbar/>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
-        </Routes>
-        <Footer/>
-    </Box>
-  )
-}
+const App = () => (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+  );
 
-export default App
+
+export default App;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,10 +4,30 @@ import {exerciseOptions, fetchData} from '../utils/fetchData';
 
 const SearchExercises = () => {
   const [search, setsearch] = useState('');
+  const [exercises, setexercises] = useState([]);
+  const [bodyParts, setbodyParts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchExercisesData = async () =>{
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+  }, [])
+  
 
   const handleSearch = async() => {
     if(search) {
-      const exercisesData = await fetchData('');
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      const searchedExercises = exercisesData.filter(
+        (exercise) => exercise.name.toLowerCase().includes(search)
+        ||exercise.target.toLowerCase().includes(search)
+        ||exercise.equipment.toLowerCase().includes(search)
+        ||exercise.bodyPart.toLowerCase().includes(search)
+      );
+      setSearch('');
+      setExercises(searchedExercises);
     }
 
   }
@@ -17,9 +37,9 @@ const SearchExercises = () => {
     <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
       <Typography fontWeight='700' sx={{fontSize: {lg: '44px', xs: '30px'}}} mb='50px' textAlign='center'>Awesome Exercises you <br />should Know</Typography>
       <Box position='relative' mb='72px'>
-        <TextField sx={{input:{ fontWeight: '700', border: 'none', borderRadius: '4px'}, width: {lg: '800px', xs: '350px'}, backgroundColor: '#fff', borderRadius: '40px'}} height='76px' value={search} onChange={(e) => setSearch(e.target.value.toLowerCase())} placeholder='Search Exercises' type='text'/>
+        <TextField sx={{input:{ fontWeight: '700', border: 'none', borderRadius: '4px'}, width: {lg: '800px', xs: '350px'}, backgroundColor: '#fff', borderRadius: '40px'}} height='76px' value={search} onChange={(e) => setsearch(e.target.value.toLowerCase())} placeholder='Search Exercises' type="text"/>
         <Button className='search-btn' sx={{bgcolor: '#123024', color: '#fff', textTransform: 'none', width: { lg: '175px', xs: '80px'}, 
-        fontSize: {lg: '20px', xs: '14px'}, height: '56px', position: 'absolute', right: '0'}} onClick='handleSearch'>Search</Button>
+        fontSize: {lg: '20px', xs: '14px'}, height: '56px', position: 'absolute', right: '0'}} onClick={handleSearch}>Search</Button>
       </Box>
     </Stack>
   )
