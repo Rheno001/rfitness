be823dda5ab33b6b8ec2d37572caf0b08d9357f2
Rheno001 <renfredreuben@gmail.com>
2024-09-15 20:40:46 +0100

    debugging scrollbar code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,13 @@ import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
+  const [bodyPart, setbodyPart] = useState('all')
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
         <HeroBanner/>
-        <SearchExercises/>
+        <SearchExercises setExercises={setExercises}
+        bodyPart={bodyPart}/>
         <Exercises/>
     </Box>
   )
